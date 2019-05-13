9d09e67c758cf837d41c7a4935533a879e1483ad
Pedro Chamberlain <pedrochmatos@gmail.com>
2019-05-12 21:15:09 -0300

    studying for exam

--- labs/lab-01/echo.c
+++ labs/lab-01/echo.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-int main(int argc, char *argv[]){
-  for (int i = 1; i < argc-1; ++i)
-      printf("%s%s", argv[i], " ");
 
-  printf("%s%s", argv[argc-1], "\n");
- 
-  return 0;
+int main (int argc, char *argv[]) {
+
+	for (int i = 1; i < argc; ++i)
+		printf("%s ", argv[i]);
+
+	printf("\n");
+
+	return 0;
 }
--- labs/lab-02/lab-04a.c
+++ labs/lab-02/lab-04a.c
@@ -7,13 +7,12 @@ int main (void) {
 	char *p;
 	int segmento, id, pid, status;
 	// aloca a memória compartilhada
-	segmento = shmget (8765, 10 * sizeof(char), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+	segmento = shmget (IPC_PRIVATE, 15 * sizeof(char), IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
 	printf("%d\n",segmento);
 	p = (char *) shmat(segmento, 0, 0); // comparar o retorno com -1
 	
 	printf("Insira uma mensagem do dia\n");
 	scanf("%[^\n]", p);
-	printf("\n%s",p);
 	
 	sleep(30);
 	// libera a memória compartilhada do processo
